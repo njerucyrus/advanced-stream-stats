112e0038a1737702b898285d21c38d24b509934c
Njeru Cyrus <njerucyrus5@hotmail.com>
2022-10-23 08:00:21 +0300

    added generate client token braintree

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -4,7 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class SubscriptionController extends Controller
 {
-    //
+    private $gateway;
+    public function __construct()
+    {
+        $this->gateway =  new \Braintree\Gateway(
+            [
+                'environment' => env('BRAINTREE_ENVIRONMENT'),
+                'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+                'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+                'privateKey' => env('BRAINTREE_PRIVATE_KEY')
+            ]
+        );
+    }
+
+    public function token(Request $request)
+    {
+        $clientToken = $this->gateway->clientToken()->generate();
+    }
 }
