6301a229ec6c5e9d6a6f93563c7235a5f1af3528
Njeru Cyrus <njerucyrus5@hotmail.com>
2022-10-26 23:45:44 +0300

    updated stream controller

--- app/Http/Controllers/StreamStatsController.php
+++ app/Http/Controllers/StreamStatsController.php
@@ -8,7 +8,10 @@ use Inertia\Inertia;
 class StreamStatsController extends Controller
 {
     public function index() {
+        //this should represent data from twitch. For now we use dammy data 
+        //so no need to have real data
         $stats = [];
+        
         return Inertia::render('StreamStats', [
             'stats'=>$stats
         ]);
