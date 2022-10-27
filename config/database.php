863c3431a2ef5d22eb2ebfadc762ec1d1abed483
Njeru Cyrus <njerucyrus5@hotmail.com>
2022-10-27 10:40:27 +0300

    added db config

--- config/database.php
+++ config/database.php
@@ -7,8 +7,7 @@ use Illuminate\Support\Str;
 // define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
 // define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
 
-
-
+$DATABASE_URL = parse_url('DATABASE_URL');
 return [
 
     /*
@@ -72,12 +71,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], '/'),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
